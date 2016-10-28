3a646ffa47fc836963b3199f865ad7f56e688e9b
ehouais <ehouais@free.fr>
2016-10-28 18:57:18 +0200

    require-compliant barchart parser

--- barchart/parser.js
+++ barchart/parser.js
@@ -1,18 +1,20 @@
-parser = {
-    parseParams: function(str) {
-        var tokens = str.split(':'),
-            cols = tokens[1].split(',');
+define(function() {
+    return {
+        parseParams: function(str) {
+            var tokens = str.split(':'),
+                cols = tokens[1].split(',');
 
-        return {
-            labels: +tokens[0]-1,
-            grouping: cols.map(function(stack) {
-                return stack.split('+').map(function(index) {
-                    return +index-1;
-                });
-            })
+            return {
+                labels: +tokens[0]-1,
+                grouping: cols.map(function(stack) {
+                    return stack.split('+').map(function(index) {
+                        return +index-1;
+                    });
+                })
+            }
+        },
+        parseData: function(str) {
+            return str.replace(/;/g, '\n').split('\n').map(function(row) { return row.split(','); });
         }
-    },
-    parseData: function(str) {
-        return str.replace(/;/g, '\n').split('\n').map(function(row) { return row.split(','); });
-    }
-};
+    };
+});
