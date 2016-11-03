f34e828dfd37785ad17fee6d0632c4f24fe010a7
ehouais <ehouais@free.fr>
2016-11-03 03:26:23 +0100

    Horizontal barchart implementation

---
+++ hbarchart/parser.js
@@ -0,0 +1,20 @@
+define(function() {
+    return {
+        parseParams: function(str) {
+            var tokens = str.split(':'),
+                cols = tokens[1].split(',');
+
+            return {
+                labels: +tokens[0]-1,
+                grouping: cols.map(function(stack) {
+                    return stack.split('+').map(function(index) {
+                        return +index-1;
+                    });
+                })
+            }
+        },
+        parseData: function(str) {
+            return str.replace(/;/g, '\n').split('\n').map(function(row) { return row.split(','); });
+        }
+    };
+});
