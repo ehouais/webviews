5fe047882691c3d1f2953ef4f4abaaa905779ab1
Philippe Deschaseaux <philippe.deschaseaux@orange.com>
2016-11-08 11:43:44 +0100

    First working version

--- linechart/linechart.js
+++ linechart/linechart.js
@@ -11,7 +11,13 @@ define(['d3', '../timeline/timescale'], function(d3, Timescale) {
             cScale = d3.scaleOrdinal(d3.schemeCategory10),
             line,
             marker,
-            svgline,
+            svgline = d3.line()
+                .x(function(d) {
+                    return tScale(d.date);
+                })
+                .y(function(d) {
+                    return vScale(d.value);
+                }),
             labels,
             legend,
             text,
@@ -28,23 +34,16 @@ define(['d3', '../timeline/timescale'], function(d3, Timescale) {
                 chart
                     .attr('transform', 'translate('+left+','+top+')');
 
-                /**xx
-                    .attr('transform', 'translate(0,' + height + ')')
-                    .call(xAxis)
-                    .selectAll('text')
-                    .style('font-size', fontsize+'px')
-                    .each(function(d, i) {
-                        var text = d3.select(this).text(null).attr('y', 4);
-                        (values.labels[i]).split('  ').forEach(function(line, l) {
-                            text.append('tspan').text(line).attr({x: 0, dy: fontsize});
-                        });
-                    });*/
-
                 vScale
                     .range([height, 0]);
 
+                tScale
+                    .range([0, width]);
+
+                timescale.redraw(width, height);
+
                 yAxis
-                    .ticks(height/25)
+                    .ticks(Math.min(11, height/25))
                     .tickSize(-width);
 
                 yy
@@ -52,29 +51,22 @@ define(['d3', '../timeline/timescale'], function(d3, Timescale) {
                     .selectAll('text')
                     .style('font-size', fontsize+'px');
 
-                yy
-                    .selectAll('g')
-                    .filter(function(d) { return d; })
-                    .classed('minor', true);
-
                 line
                     .attr('d', svgline)
                     .attr('stroke-width', fontsize/5);
 
-                if (labels) {
-                    legend
-                        .attr('transform', function(d, i) { return 'translate(0,'+(-(i+2)*fontsize)+')'; });
+                legend
+                    .attr('transform', function(d, i) { return 'translate(0,'+(-(i+2)*fontsize)+')'; });
 
-                    lrects
-                        .attr('x', width-18)
-                        .attr('y', fontsize/2)
-                        .attr('height', fontsize/5);
+                lrects
+                    .attr('x', width-18)
+                    .attr('y', fontsize/2)
+                    .attr('height', fontsize/5);
 
-                    ltexts
-                        .attr('x', width-24)
-                        .attr('y', fontsize/2)
-                        .style('font-size', fontsize+'px');
-                }
+                ltexts
+                    .attr('x', width-24)
+                    .attr('y', fontsize/2)
+                    .style('font-size', fontsize+'px');
 
                 return svg._groups[0][0].getBBox();
             };
@@ -93,61 +85,56 @@ define(['d3', '../timeline/timescale'], function(d3, Timescale) {
                     });
                 });
 
-                tScale.domain([start, end]);
-                vScale.domain([0, max]);
+                var tmargin = Math.max((end-start)/20, 24*60*60);
+                tScale
+                    .domain([start-tmargin, end+tmargin]);
 
-                svgline = d3.line()
-                    .x(function(d, i) { return tScale(i); })
-                    .y(function(d) { return vScale(d); })
-                    .defined(function(d) { return d != null; });
+                vScale
+                    .domain([params.offset, max]);
 
-                line = lines
-                    .selectAll('.line')
+                line = lines.selectAll('.line')
                     .data(series)
                     .enter()
                     .append('path')
+                    .datum(function(d) {
+                        return d.values;
+                    })
                     .attr('class', 'line')
                     .attr('stroke', function(d, i) { return cScale(i); })
                     .attr('marker-start', function(d, i) { return 'url(#marker_'+i+')'; })
                     .attr('marker-mid', function(d, i) { return 'url(#marker_'+i+')'; })
                     .attr('marker-end', function(d, i) { return 'url(#marker_'+i+')'; });
 
-                /*marker = svg.append('defs').selectAll('marker')
+                marker = svg.append('defs').selectAll('marker')
                     .data(series)
                     .enter()
                     .append('marker')
-                    .attr({
-                        id: function(d, i) { return 'marker_'+i; },
-                        markerWidth: 8,
-                        markerHeight: 8,
-                        refX: 5,
-                        refY: 5
-                    })
+                    .attr('id', function(d, i) { return 'marker_'+i; })
+                    .attr('markerWidth', 8)
+                    .attr('markerHeight', 8)
+                    .attr('refX', 5)
+                    .attr('refY', 5)
                     .append('circle')
-                    .attr({
-                        cx: 5,
-                        cy: 5,
-                        r: 1.2,
-                        fill: function(d, i) { return cScale(i); },
-                        class: 'circleMarker'
-                    });*/
+                    .attr('cx', 5)
+                    .attr('cy', 5)
+                    .attr('r', 1.2)
+                    .attr('fill', function(d, i) { return cScale(i); })
+                    .classed('circleMarker');
 
-                /*if (labels) {
-                    legend = chart.append('g').selectAll('.legend')
-                        .data(labels)
-                        .enter()
-                        .append('g')
-                        .attr('class', 'legend');
+                legend = chart.append('g').selectAll('.legend')
+                    .data(series)
+                    .enter()
+                    .append('g')
+                    .attr('class', 'legend');
 
-                    lrects = legend.append('rect')
-                        .attr('width', 18)
-                        .style('fill', function(d, i) { return cScale(i); });
+                lrects = legend.append('rect')
+                    .attr('width', 18)
+                    .style('fill', function(d, i) { return cScale(i); });
 
-                    ltexts = legend.append('text')
-                        .attr('dy', '.35em')
-                        .style('text-anchor', 'end')
-                        .text(function(d) { return d; });
-                }*/
+                ltexts = legend.append('text')
+                    .attr('dy', '.35em')
+                    .style('text-anchor', 'end')
+                    .text(function(d) { return d.label; });
 
                 this.resize();
             },
--- linechart/parser.js
+++ linechart/parser.js
@@ -1,30 +1,22 @@
 define(function() {
     return {
         parseParams: function(str) {
+            return {offset: +str};
         },
         parseData: function(str) {
-            /*var str = str.replace(/_/g, ' ');*/
-
-            // get serie labels
-            /*offset = 0;
-            if (res = str.match(/^\{([^\}|]*)(\|([^\}]+))?\}(.*)/)) {
-                if (res[1]) labels = res[1].split(',');
-                if (res[3]) offset = res[3];
-                str = res[4];
-            }*/
-
             var offset = new Date().getTimezoneOffset()*60000;
             // get serie values
-            return str.split('|').map(function(series) {
+            return str.split('|').map(function(series, i) {
+                var match = series.match(/^([^:]+):/);
                 return {
-                    label: 'toto',
+                    label: match ? match[1] : 'series '+i,
                     values: series.split(';').map(function(pair) {
                         var tokens = pair.split(','),
                             year = (new Date()).getFullYear(),
                             match = tokens[0].match(/(\d{2})\/(\d{2})(\/(\d{2}))?/);
 
                         return {
-                            date: Date.parse((match[3] ? match[4] : year)+'-'+match[2]+'-'+match[1]) + offset,
+                            date: new Date((match[3] ? +match[4]+2000 : year)+'-'+match[2]+'-'+match[1])/* + offset*/,
                             value: +tokens[1]
                         };
                     })
